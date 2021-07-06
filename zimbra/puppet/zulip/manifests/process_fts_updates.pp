d64117c16548cca9fc70b51d52c89fdecab3717b
abhishek <abhishekdeshmukh.com@gmail.com>
2021-07-07 05:15:52 +0530

    puppet correction and postgres corrections

---
+++ zimbra/puppet/zulip/manifests/process_fts_updates.pp
@@ -0,0 +1,35 @@
+class zulip::process_fts_updates {
+  include zulip::supervisor
+  case $::osfamily {
+    'debian': {
+      $fts_updates_packages = [
+        # Needed to run process_fts_updates
+        'python3-psycopg2', # TODO: use a virtualenv instead
+      ]
+      zulip::safepackage { $fts_updates_packages: ensure => 'installed' }
+    }
+    'redhat': {
+    }
+    default: {
+      fail('osfamily not supported')
+    }
+  }
+
+  file { '/usr/local/bin/process_fts_updates':
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+    source => 'puppet:///modules/zulip/postgresql/process_fts_updates',
+  }
+
+  file { "${zulip::common::supervisor_conf_dir}/zulip_db.conf":
+    ensure  => file,
+    require => Package[supervisor],
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    source  => 'puppet:///modules/zulip/supervisor/conf.d/zulip_db.conf',
+    notify  => Service[$zulip::common::supervisor_service],
+  }
+}
